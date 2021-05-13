326c0750ef54b2098130f4146cd3c67bcf574441
Watchapon Jun-o-pat <watchapon@igeargeek.com>
2021-05-13 18:43:48 +0700

    shuffle

--- basic/deck.go
+++ basic/deck.go
@@ -1,12 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"time"
+)
 
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Heart", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
 
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
 
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0777)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error!", err)
+	}
+	// 1. []byte -> string
+	// 2. string -> []string
+	// 3. []string -> deck
+	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- basic/main.go
+++ basic/main.go
@@ -1,26 +1,11 @@
 package main
 
 func main() {
-	// cards := deck{"Ace of Diamonds", newCard()}
-	// cards = append(cards, "Six of Spades")
-	// cards.print()
-
-	cards := deck{}
-	cardSuits := []string{"Spades", "Hearts", "Diamonds"}
-	cardValues := []string{"Ace", "Two", "Three"}
-
-	for _, cardSuit := range cardSuits {
-		// fmt.Println(cardSuit)
-		for _, cardValue := range cardValues {
-			// fmt.Println(cardValue + " of " + cardSuit)
-			cards = append(cards, cardValue+" of "+cardSuit)
-		}
-	}
-
+	cards := newDeckFromFile("deck.txt")
 	cards.print()
-
-}
-
-func newCard() string {
-	return "Ace of Spades"
+	cards.shuffle()
+	cards.shuffle()
+	cards.shuffle()
+	cards.shuffle()
+	cards.print()
 }
